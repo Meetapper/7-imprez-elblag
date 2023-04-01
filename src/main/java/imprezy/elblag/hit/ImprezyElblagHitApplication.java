0b4715ed247d3cf95c55c47f16ff598cec3c9292
Patryk Skupien <pskupien@akamai.com>
2023-04-01 11:21:14 +0200

    first commit

---
+++ src/main/java/imprezy/elblag/hit/ImprezyElblagHitApplication.java
@@ -0,0 +1,13 @@
+package imprezy.elblag.hit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprezyElblagHitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImprezyElblagHitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/imprezy/elblag/hit/ImprezyElblagHitApplicationTests.java
@@ -0,0 +1,13 @@
+package imprezy.elblag.hit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprezyElblagHitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
